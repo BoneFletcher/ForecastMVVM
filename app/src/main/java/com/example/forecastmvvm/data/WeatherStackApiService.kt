c3935bfbdd2bf1e3e76faad734e3574e614501d5
Android <gilad@radiomize.com>
2019-10-04 19:15:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/forecastmvvm/data/WeatherStackApiService.kt
@@ -0,0 +1,49 @@
+package com.example.forecastmvvm.data
+
+import com.example.forecastmvvm.data.response.CurrentWeatherResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY = "e2acd743a762c85f84e9407fe8adaca1"
+//http://api.weatherstack.com/current?access_key=e2acd743a762c85f84e9407fe8adaca1&query=Kharkiv&lang=en
+interface WeatherStackApiService {
+
+    @GET("current?")
+    fun getCurrentWeather(
+        @Query("query") location: String
+    ): Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke(): WeatherStackApiService {
+            val requestInterceptor = Interceptor{chain ->
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("http://api.weatherstack.com/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherStackApiService::class.java)
+        }
+    }
+}
